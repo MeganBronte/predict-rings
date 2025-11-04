cdb887febfc50a39e88833fc4d679c99340c8038
MeganBronte <worldburicze@outlook.com>
2025-11-04 16:30:00 +0800

    feat: improve account task automation

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,4 +1,4 @@
-import { task } from "hardhat/config";
+﻿import { task } from "hardhat/config";
 
 task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -7,3 +7,4 @@ task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
     console.log(`${account.address}`);
   }
 });
+
